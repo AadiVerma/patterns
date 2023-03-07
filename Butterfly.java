71a0451d01337f3c0b36ee0c0b1827075c5e6bae
AadiVerma <122523624+AadiVerma@users.noreply.github.com>
2023-03-07 11:12:44 +0530

    Add files via upload

---
+++ Butterfly.java
@@ -0,0 +1,34 @@
+// import java.util.*;
+// public class Butterfly{
+//     public static void main(String args[]){
+//         Scanner av=new Scanner(System.in);
+//         System.out.print("Enter no: ");
+//         int n=av.nextInt();
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print('*');
+//             }
+//                 for(int m=1;m<=n-i;m++){
+//                     System.out.print(" ");
+//                 }
+//                 for(int o=1;o<=n-i;o++){
+//                     System.out.print(" ");
+//                 }for(int l=1;l<=i;l++){
+//                     System.out.print('*');
+//                 }System.out.println();
+    
+//             }
+//             for(int i=1;i<=n;i++){
+//                 for(int j=i;j<=n;j++){
+//                     System.out.print('*');
+//                 }for(int k=0;k<=i-2;k++){
+//                     System.out.print(" ");}
+//                     for(int l=0;l<=i-2;l++){
+//                         System.out.print(" ");
+//                     }for(int m=i;m<=n;m++){
+//                         System.out.print('*');
+//                     }System.out.println();
+//                 }
+//             }
+//         }
+        
